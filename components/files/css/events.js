bf6440d0f71e8fbdcfae6cb479847914140b1428
kakegawa <pedestrian.deck.2023@gmail.com>
2023-12-18 03:36:43 +0900

    ナビゲーションの更新

--- components/files/css/events.js
+++ components/files/css/events.js
@@ -1,19 +1,3 @@
-/* コンポーネント：menu */
-if (document.getElementsByClassName('c_menu')) {
-    const menu = document.getElementsByClassName('c_menu');
-    for (let i = 0; i < menu.length; i++) {
-        menu[i].lastElementChild.addEventListener('click', function () {
-            if (this.parentElement.classList.contains('c_menu_close')) {
-                this.parentElement.classList.remove('c_menu_close');
-                this.parentElement.classList.add('c_menu_open');
-            } else {
-                this.parentElement.classList.remove('c_menu_open');
-                this.parentElement.classList.add('c_menu_close');
-            }
-        });
-    }
-}
-
 /* コンポーネント：Counter */
 if (document.getElementById('c_counter_time')) {
     const counter = document.getElementById('c_counter_time');
@@ -34,15 +18,14 @@ if (document.getElementsByClassName('c_nav')) {
     for (let i = 0; i < nav.length; i++) {
         // バーエリア押下時の処理
         nav[i].getElementsByClassName('c_nav_barArea')[0].addEventListener('click', function () {
-            if (this.parentElement.classList.contains('c_nav_open')) {
-                // 既に開いている場合
-                // バーエリアを閉じる
-                this.parentElement.classList.remove('c_nav_open');
-            } else {
-                // 閉じている場合
-                // バーエリアを開く
-                this.parentElement.classList.add('c_nav_open');
-            }
+            // バーエリアを開く
+            this.parentElement.classList.add('c_nav_open');
+        });
+
+        // メニューエリア押下時の処理
+        nav[i].getElementsByClassName('c_nav_menuArea')[0].addEventListener('click', function () {
+            // バーエリアを閉じる
+            this.parentElement.classList.remove('c_nav_open');
         });
     }
 }
