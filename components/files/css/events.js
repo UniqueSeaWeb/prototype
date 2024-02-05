64ec49c32bf83612321c58553810235480a1dcce
kakegawa <pedestrian.deck.2023@gmail.com>
2024-02-05 19:38:39 +0900

    ダイアログ（背景まで）

--- components/files/css/events.js
+++ components/files/css/events.js
@@ -29,3 +29,43 @@ if (document.getElementsByClassName('c_nav')) {
         });
     }
 }
+
+/* コンポーネント：Dialog */
+if (document.getElementsByClassName('c_dialog')) {
+    // ダイアログが存在する場合
+    // ダイアログを開く関数
+    function showDialog(dialog) {
+        dialog.show();
+    }
+
+    // ダイアログを閉じる関数
+    function closeDialog(dialog) {
+        dialog.close();
+    }
+
+    const dialog = document.getElementsByClassName('c_dialog');
+
+    for (let i = 0; i < dialog.length; i++) {
+        // ダイアログターゲット押下時の処理
+        dialog[i].getElementsByClassName('c_dialog_target')[0].addEventListener('click', function () {
+            if (dialog[i].getElementsByClassName('c_dialog_contents')[0].open) {
+                // ダイアログコンテンツを非表示
+                closeDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+            } else {
+                // ダイアログコンテンツを表示
+                showDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+            }
+        });
+
+        // ダイアログ押下時の処理
+        dialog[i].getElementsByClassName('c_dialog_contents')[0].addEventListener('click', function () {
+            if (dialog[i].getElementsByClassName('c_dialog_contents')[0].open) {
+                // ダイアログコンテンツを非表示
+                closeDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+            } else {
+                // ダイアログコンテンツを表示
+                showDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+            }
+        });
+    }
+}
