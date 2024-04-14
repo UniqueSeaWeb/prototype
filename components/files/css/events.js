c8959af5c6bea2ab97a526665423380abf641f8d
kakegawa <pedestrian.deck.2023@gmail.com>
2024-04-14 18:18:54 +0900

    ダイアログの実装

--- components/files/css/events.js
+++ components/files/css/events.js
@@ -22,6 +22,24 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+/* コンポーネント：Carousel */
+if (document.getElementsByClassName('c_carousel')) {
+    const carousel = document.getElementsByClassName('c_carousel');
+    for (let i = 0; i < carousel.length; i++) {
+        const dot = carousel[i].getElementsByClassName('c_carousel_dot');
+
+        for (let j = 0; j < dot.length; j++) {
+            dot[j].addEventListener('click', function () {
+                if (!this.classList.contains('c_carousel_active')) {
+                    const activeDot = this.parentElement.parentElement.getElementsByClassName('c_carousel_active')[0];
+                    activeDot.classList.remove('c_carousel_active');
+                    this.classList.add('c_carousel_active');
+                }
+            });
+        }
+    }
+}
+
 /* コンポーネント：Navigation */
 if (document.getElementsByClassName('c_nav')) {
     // ナビゲーションが存在する場合
@@ -61,24 +79,42 @@ if (document.getElementsByClassName('c_dialog')) {
     for (let i = 0; i < dialog.length; i++) {
         // ダイアログターゲット押下時の処理
         dialog[i].getElementsByClassName('c_dialog_target')[0].addEventListener('click', function () {
-            if (dialog[i].getElementsByClassName('c_dialog_contents')[0].open) {
+            if (dialog[i].getElementsByClassName('c_dialog_backGround')[0].open) {
                 // ダイアログコンテンツを非表示
-                closeDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+                closeDialog(dialog[i].getElementsByClassName('c_dialog_backGround')[0]);
             } else {
                 // ダイアログコンテンツを表示
-                showDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+                showDialog(dialog[i].getElementsByClassName('c_dialog_backGround')[0]);
+            }
+        });
+
+        // ×ボタン押下時の処理
+        dialog[i].getElementsByClassName('c_dialog_closeIcon')[0].addEventListener('click', function () {
+            // ダイアログコンテンツを非表示
+            closeDialog(dialog[i].getElementsByClassName('c_dialog_backGround')[0]);
+        });
+
+        // ×ボタンEnter時の処理
+        dialog[i].getElementsByClassName('c_dialog_closeIcon')[0].firstElementChild.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') {
+                this.parentElement.click();
             }
         });
 
         // ダイアログ押下時の処理
-        dialog[i].getElementsByClassName('c_dialog_contents')[0].addEventListener('click', function () {
-            if (dialog[i].getElementsByClassName('c_dialog_contents')[0].open) {
+        dialog[i].getElementsByClassName('c_dialog_backGround')[0].addEventListener('click', function () {
+            if (dialog[i].getElementsByClassName('c_dialog_backGround')[0].open) {
                 // ダイアログコンテンツを非表示
-                closeDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+                closeDialog(dialog[i].getElementsByClassName('c_dialog_backGround')[0]);
             } else {
                 // ダイアログコンテンツを表示
-                showDialog(dialog[i].getElementsByClassName('c_dialog_contents')[0]);
+                showDialog(dialog[i].getElementsByClassName('c_dialog_backGround')[0]);
             }
+        });
+
+        // コンテンツのクリックイベントを防止
+        dialog[i].getElementsByClassName('c_dialog_contents')[0].addEventListener('click', function (e) {
+            e.stopPropagation();
         });
     }
 }
