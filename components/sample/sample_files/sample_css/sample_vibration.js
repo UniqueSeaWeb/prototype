b987582b7e4a9244b385830231251ce6339a77e1
uniquesea <uniquesea912@gmail.com>
2024-06-10 00:48:18 +0900

    vibration2

--- components/sample/sample_files/sample_css/sample_vibration.js
+++ components/sample/sample_files/sample_css/sample_vibration.js
@@ -3,8 +3,13 @@ import * as Vibrate from 'vibrate';
 
 window.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('sp_btn');
-
+    const btn2 = document.getElementById('sp_btn2');
+    
     btn.addEventListener('click', function () {
         Vibrate.simpleTop();
+    });
+
+    btn2.addEventListener('click', function () {
+        window.navigator.vibrate(200);
     });
 });
